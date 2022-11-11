a1dee98eabc780b1e41ffaa085a6c72a40d62eb1
Yesa <79618650+pinangsor1@users.noreply.github.com>
2022-11-11 11:58:43 +0700

    Create perulangan1.cpp

---
+++ perulangan1.cpp
@@ -0,0 +1,19 @@
+#include<iostream.h>
+#include<conio.h>
+
+main()
+{
+  int bilangan, jumlah, x; //mendeklarasikan jenis tipe data dari setiap variabel
+  float rata;              
+  jumlah = o    //jumlah didefenisikan 0 dahulu
+  for (x=1; x<=5; x++) //perulangan pada bahasa c++
+  {
+    cout<<"masukkan bilangan : ";
+    cin>>bilangan;
+    jumlah = jumlah + bilangan;
+  }
+  rata=jumlah/6
+  cout<<"jumlah bilangan adalah : "<<bilangan<<endl;
+  cout<<"rata rata bilangan adalah : "<<rata;
+  getch(); //agar layar terminal nya tidak langsung keluar
+}
