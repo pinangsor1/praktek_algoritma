5c901c456f40361f4b5f8b7ce2d909375c50856f
Yesa <79618650+pinangsor1@users.noreply.github.com>
2022-11-17 21:05:39 -0800

    Create for-bersarang.cpp

---
+++ for-bersarang.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>
+#include<iomanip.h>
+#include<conio.h>
+
+main()
+{
+	int i, j;
+   for (j=1; j<=6; j++)
+   {
+   	for (i=1; i<=10; i++)
+      {
+        cout<<i;
+      }
+      cout<<endl;
+   }
+   getch();
+}
